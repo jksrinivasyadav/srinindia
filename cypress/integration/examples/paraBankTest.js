32ca5b184391d57e34e7d7fe562b1808a304d3b4
Sri <chetlapalli.6353madhu@gmail.com>
2021-03-29 06:54:54 +0100

    CTSMTH-12129 Tests

--- cypress/integration/examples/paraBankTest.js
+++ cypress/integration/examples/paraBankTest.js
@@ -6,6 +6,6 @@ context('Parabank site access', () => {
         bank.goToBankSite()
     })
     it('check title of the page', () => {
-        bank.verifyTitle('ParaBank | Welcome | Online Banki')
+        bank.verifyTitle('ParaBank | Welcome | Online Banking')
     })
 })
